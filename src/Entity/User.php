d97cea717740885d3c6acce0488e78bdb9e9ceca
syl20ko <sylvain.caron@baliztic.com>
2019-05-12 09:45:41 +0200

    entity article

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Webmozart\Assert\Assert;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -46,6 +48,16 @@ class User
      * @ORM\OneToOne(targetEntity="App\Entity\Entreprise", mappedBy="leader", cascade={"persist", "remove"})
      */
     private $entreprise;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="author", orphanRemoval=true)
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -124,6 +136,37 @@ class User
         // set the owning side of the relation if necessary
         if ($this !== $entreprise->getLeader()) {
             $entreprise->setLeader($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Article[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Article $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            // set the owning side to null (unless already changed)
+            if ($article->getAuthor() === $this) {
+                $article->setAuthor(null);
+            }
         }
 
         return $this;
