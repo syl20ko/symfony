0661ffebc13ad938c4b7e46e9afae4a3af5573a9
syl20ko <sylvain.caron@baliztic.com>
2019-05-13 20:37:45 +0200

    profil page update

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -6,18 +6,22 @@ use App\Entity\Entreprise;
 use App\Repository\EntrepriseRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 
 class EntrepriseController extends AbstractController
 {
     /**
      * @Route("/entreprise/{slug}", name="entreprise_show")
      */
-    public function show($slug, EntrepriseRepository $repo)
+    public function show($slug, EntrepriseRepository $repo, ArticleRepository $articlerepo)
     {
         $entreprise = $repo->findOneBySlug($slug);
 
+        $article = $articlerepo->findByAuthor($entreprise);
+
         return $this->render('entreprise/show.html.twig', [
             'entreprise' => $entreprise,
+            'article' => $article
         ]);
     }
 }
